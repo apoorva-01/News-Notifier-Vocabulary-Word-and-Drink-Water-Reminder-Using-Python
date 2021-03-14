501af8d2e91b8d031749b9db20a9268d17c5b376
Apoorva Verma <76636952+apoorva-01@users.noreply.github.com>
2021-03-14 15:23:43 +0530

    Update DrinkWaterReminder.py

--- DrinkWaterReminder.py
+++ DrinkWaterReminder.py
@@ -6,7 +6,7 @@ from plyer import notification
  	while True:
  		notification.notify(
  			title = "**Please Drink Water Now!!",
- 			message ="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+ 			message ="Please Drink Water",
  			app_icon = "path to your .ico file",
  			timeout= 12
  			)
